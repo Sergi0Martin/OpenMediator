9bc291537571d36aff7368cb618a06927eb8543e
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2025-04-10 18:52:55 +0200

    Add middleware support (#1)
    Add functionality for configuring and executing custom middlewares before or after commands.

---
+++ examples/WebApplicationAPI/MediatorMiddleware/CustomMediatorMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using OpenMediator;
+using OpenMediator.Middlewares;
+
+namespace WebApplicationAPI.MediatorMiddleware;
+
+public class CustomMediatorMiddleware(ILogger<CustomMediatorMiddleware> _logger) : IMediatorMiddleware
+{
+    public async Task ExecuteAsync<TCommand>(TCommand command, Func<Task> next)
+        where TCommand : ICommand
+    {
+        // Do something before the command
+        _logger.LogInformation("=========> Before command: {Command}", command.GetType().Name);
+        Task.Delay(1000).Wait();
+
+        await next();
+
+        // Do something after the command
+        _logger.LogInformation("=========> After command: {Command}", command.GetType().Name);
+        Task.Delay(1000).Wait();
+
+    }
+}
