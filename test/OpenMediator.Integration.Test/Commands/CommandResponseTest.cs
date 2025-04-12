71919ff06ca10edf07d06c84883fbbee6690162f
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2025-04-13 01:51:00 +0200

    Add tests and fix Command with response
    Command with response when middleware configured was not working properly

---
+++ test/OpenMediator.Integration.Test/Commands/CommandResponseTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenMediator.Buses;
+using OpenMediator.Shared.Integration.Test;
+using OpenMediator.Shared.Integration.Test.Commands;
+
+namespace OpenMediator.Integration.Test.Commands;
+
+public sealed class CustomMediatorMiddlewareTest
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IMediatorBus _mediator;
+    private TestDependency _testDependency;
+
+    public CustomMediatorMiddlewareTest()
+    {
+        _testDependency = new TestDependency();
+        var services = new ServiceCollection();
+        services.AddOpenMediator(config =>
+        {
+            config.RegisterCommandsFromAssembly(typeof(GetUserCommandHandler).Assembly);
+        });
+        services.AddSingleton(_testDependency);
+        _serviceProvider = services.BuildServiceProvider();
+        _mediator = _serviceProvider.GetService<IMediatorBus>()!;
+    }
+
+    [Fact]
+    public async Task Send_Command_With_Response_Works()
+    {
+        // Arrange
+        var command = new GetUserCommand(1);
+
+        // Act
+        var result = await _mediator.SendAsync<GetUserCommand, User>(command);
+
+        // Assert
+        var expectedResult = new User(
+            Id: 1,
+            Name: "Ralph",
+            Email: "ralp@company.com");
+        Assert.Equal(result, expectedResult);
+        Assert.True(_testDependency.Called);
+    }
+}
---
+++ test/OpenMediator.Shared.Integration.Test/CustomMediatorMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using OpenMediator.Middlewares;
+
+namespace OpenMediator.Shared.Integration.Test;
+
+public class CustomMediatorMiddleware(TestDependency testDependency) : IMediatorMiddleware
+{
+    public async Task ExecuteAsync<TCommand>(TCommand command, Func<Task> next)
+        where TCommand : ICommand
+    {
+        testDependency.Called = true;
+
+        // Do something before the command
+        testDependency.Counter++;
+        Task.Delay(500).Wait();
+
+        await next();
+
+        // Do something after the command
+        testDependency.Counter++;
+        Task.Delay(500).Wait();
+    }
+
+    public Task ExecuteAsync<TCommand, TResponse>(TCommand command, Func<Task<TResponse>> next) where TCommand : ICommand<TResponse>
+    {
+        // Do something before the command
+        testDependency.Counter++;
+        Task.Delay(500).Wait();
+
+        var result = next();
+
+        // Do something after the command
+        testDependency.Counter++;
+        Task.Delay(500).Wait();
+
+        return result;
+    }
+}
